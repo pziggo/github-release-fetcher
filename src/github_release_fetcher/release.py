ba839e52b9eb824504773a791c2afe67bc4003cc
Patrick Ziegler <ziggo@zaebos.de>
2020-11-08 17:24:32 +0100

    Print all assets on command line

--- src/github_release_fetcher/release.py
+++ src/github_release_fetcher/release.py
@@ -1,5 +1,6 @@
 """Data package to represent the release information."""
 from typing import Dict
+from typing import List
 
 
 class Release:
@@ -8,8 +9,21 @@ class Release:
     def __init__(self, data: Dict) -> None:
         """Release constructor."""
         self._tag_name: str = data["tag_name"]
+        self._assets: List[str] = []
+
+        for asset in data["assets"]:
+            self._assets.append(asset["browser_download_url"])
 
     @property
     def tag_name(self) -> str:
         """Return tag_name attribute."""
         return self._tag_name
+
+    @property
+    def assets(self) -> List[str]:
+        """Return list of download URLS."""
+        return self._assets
+
+    def asset_size(self) -> int:
+        """Return number or assets."""
+        return len(self._assets)
--- tests/github_test.py
+++ tests/github_test.py
@@ -6,6 +6,7 @@ import pytest
 
 from github_release_fetcher import github
 from github_release_fetcher.release import Release
+from .test_data import dummy_asset_1
 
 
 def test_latest_release_uses_given_repository(mock_requests_get: Mock) -> None:
@@ -26,3 +27,15 @@ def test_latest_release_handles_validation_errors(mock_requests_get: Mock) -> No
     mock_requests_get.return_value.__enter__.return_value.json.return_value = None
     with pytest.raises(click.ClickException):
         github.latest_release()
+
+
+def test_latest_release_returns_assets(mock_requests_get: Mock) -> None:
+    """It returns a list of assets."""
+    release = github.latest_release()
+    assert release.asset_size() == 2
+
+
+def test_latest_release_returns_dl_url(mock_requests_get: Mock) -> None:
+    """It returns the URL from the mock."""
+    release = github.latest_release()
+    assert release.assets[0] == dummy_asset_1
